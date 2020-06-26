97de078b6c5b6a68a51dc72ff85e68a62dc19acd
Drollbaer <marcosax95@web.de>
2020-06-26 18:37:34 +0200

    bla

--- src/mysql.php
+++ src/mysql.php
@@ -6,7 +6,11 @@ class mysql{
         $db = new mysqli("localhost", "Admin", "root", "onlinesopv2");
         if ($db->connect_errno) {
             die("Verbindung fehlgeschlagen: " . $db->connect_error);
+        }elseif (! $db){
+            printf("Verbindung fehlgeschlagen: %s\n", mysqli_connect_error());
+            exit();
         }
+
         return $db;
     }
 
