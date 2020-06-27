2b1e7786a9f289f0a762ac2f34111ae17465e71f
fischech123 <cfische2@stud.hs-offenburg.de>
2020-06-27 22:41:27 +0200

    fehler db anbindung behoben

--- src/formular_verarbeiten.php
+++ src/formular_verarbeiten.php
@@ -11,42 +11,42 @@ $stadt = $_POST['Stadt'];
 $strasse = $_POST['Strasse'];
 $land = $_POST['Land'];
 
-$sql = "SELECT * FROM konto WHERE Email = ? OR Benutzername = ?";
-$smt = $db->prepare($sql);
-$smt->bind_param('ss',$_POST['Email'], $_POST['Benutzername']);
-$smt->execute();
-$checkUser = $smt->num_rows();
+$sql = "SELECT * FROM konto WHERE Email = '".$_POST['Email']."' OR Benutzername = '".$_POST['Benutzername']."'";
+$result = mysqli_query($db,$sql);
 
+while ($row = mysqli_fetch_array($result)) {
+    if($row['Benutzername'] != '' or $row['Email'] !=''){
 
-
-if($checkUser > 0){
-    $smt->close();
-    echo "<a href=View/anmeldung.html ";
+        header('Location: /anmelden');
+die();
+    }
 }
 
-$conn = new mysqli("localhost", "Admin", "root", "onlinesopv2");
+
+
+    $conn = new mysqli("localhost", "Admin", "root", "onlinesopv2");
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-$sql = "INSERT INTO konto (Benutzername, Password, Email, Vorname, Nachname, Strasse, Stadt, Land)
+    $sql = "INSERT INTO konto (Benutzername, Password, Email, Vorname, Nachname, Strasse, Stadt, Land)
 VALUES (?,?,?,?,?,?,?,?)";
 
-$smt = $conn->prepare($sql);
+    $smt = $conn->prepare($sql);
 
-$smt->bind_param('ssssssss',$_POST['Benutzername'],$_POST['Password'],$_POST['Email'], $_POST['Vorname'], $_POST['Nachname'], $_POST['Strasse'], $_POST['Stadt'], $_POST['Land']);
+    $smt->bind_param('ssssssss', $_POST['Benutzername'], $_POST['Password'], $_POST['Email'], $_POST['Vorname'], $_POST['Nachname'], $_POST['Strasse'], $_POST['Stadt'], $_POST['Land']);
 
-if ($smt->execute() === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $smt->error;
-}
+    if ($smt->execute() === TRUE) {
+        echo "New record created successfully";
+        header("Location:/");
+    } else {
+        echo "Error: " . $sql . "<br>" . $smt->error;
+    }
 
-$conn->close();
+    $conn->close();
 
-echo "Funktioniert?";
-echo "Variable " . $email;
-echo "POST-Variable " . $_POST['Benutzername'];
-echo "<a href=View/login.html ";
+
+
+
 ?>
