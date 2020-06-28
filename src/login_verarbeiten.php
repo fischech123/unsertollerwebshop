3843d20c006676b8ee7e8b5a970c9b30b36c32a2
fischech123 <cfische2@stud.hs-offenburg.de>
2020-06-28 16:40:43 +0200

    login

--- src/login_verarbeiten.php
+++ src/login_verarbeiten.php
@@ -11,7 +11,7 @@ $sql = "SELECT * FROM konto WHERE Email = '" . $_POST['Email'] . "' and Password
 $result = mysqli_query($db, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
-    if ($row['Benutzername'] == '' ) {
+    if ($row['Email'] == '' ) {
 
         header('Location: /login');
         die();
