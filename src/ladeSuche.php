9e6da6584cd101c2d76f1d47866de4b4e549f984
Drollbaer <marcosax95@web.de>
2020-06-30 18:34:28 +0200

    LIKE suche verbessert

--- src/ladeSuche.php
+++ src/ladeSuche.php
@@ -2,7 +2,8 @@
     session_start();
     $db = new mysqli("localhost", "Admin", "root", "onlinesopv2");
 
-    $sql = "SELECT * FROM produkt WHERE Beschreibung LIKE '%".$_POST['Text']."%' OR Bezeichnung LIKE '%".$_POST['Text']."%'";
+    $_POST['Text'] = strtoupper($_POST['Text']);
+    $sql = "SELECT * FROM produkt WHERE upper(Beschreibung) LIKE '%".$_POST['Text']."%' OR upper(Bezeichnung) LIKE '%".$_POST['Text']."%'";
     $result = mysqli_query($db,$sql);
 
 
