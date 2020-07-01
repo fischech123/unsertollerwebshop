25fad42d53c7470ef8d853b1f745e30f1000428d
fischech123 <cfische2@stud.hs-offenburg.de>
2020-07-01 13:44:46 +0200

    login

--- src/index.php
+++ src/index.php
@@ -37,13 +37,9 @@ $router->get('/warenkorb', function() {
 });
 
 
-$router->get('/profile', function($request) {
-    return <<<HTML
-  <h1>Profile</h1>
-HTML;
+$router->get('/logout', function() {
+
+
+    include 'logout.php';
 });
 
-$router->post('/data', function($request) {
-
-    return json_encode($request->getBody());
-});
